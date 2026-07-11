82065981d10e9dec8050d85a5c656154af908301
Souvik Kumar Khan <souvik.khan@ymail.com>
2026-07-11 10:53:41 -0500

    Extended lus_ls config with globals

---
+++ after/lsp/lua_ls.lua
@@ -0,0 +1,19 @@
+--@type vim.lsp.config
+return {
+  settings = {
+    Lua = {
+      workspace = {
+        library = {
+          "$VIMRUNTIME/lua",
+          "$VIMRUNTIME/lua/vim/lsp",
+          "$VIMRUNTIME/lua/vim/treesitter",
+          vim.fn.stdpath("data") .. "/lazy/snacks.nvim",
+        },
+        checkThirdParty = false,
+      },
+      diagnostics = {
+        globals = { "vim", "Snacks" }
+      },
+    },
+  },
+}
